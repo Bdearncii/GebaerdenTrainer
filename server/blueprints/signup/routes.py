66faabc058741d353576f24c990c36044d8720ef
Daniel Ferenci <210192@studierende.htl-donaustadt.at>
2025-01-06 13:08:53 +0100

    Implementierung der Registrierung

--- server/blueprints/signup/routes.py
+++ server/blueprints/signup/routes.py
@@ -3,25 +3,27 @@ from contextlib import nullcontext
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 
-login_bp = Blueprint('login', __name__)
+signup_bp = Blueprint('login', __name__)
 
 users = {}
 
 # Todo: Firebase Integration
-@login_bp.route('/signup', methods=['POST'])
+@signup_bp.route('/signup', methods=['POST'])
 def signup():
     # Empfange JSON-Daten vom Client
     data = request.get_json()
 
     # Benutzername und Passwort aus dem Request erhalten
+    email=data.get('email')
     username = data.get('username')
     password = data.get('password')
 
-    if username in users:
+
+    if email in users:
         # Benutzername exestiert bereits
-        return jsonify({"error": "Username already exists"}), 409
+        return jsonify({"error": "User already exists"}), 409
 
     # Registrierung erfolgreich
-    users[username] = password
+    users[email] = username, password
     return jsonify({"message": "Successfull SignUp"}), 201
 
