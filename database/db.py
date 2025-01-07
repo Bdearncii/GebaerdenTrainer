8788adc1d34c6db4ee7671ba8ed8b766ce4f22de
Mahdi Afzali <210099@studierende.htl-donaustadt.at>
2025-01-07 12:21:44 +0100

    SQL-Datenbank implementierung entfernt, Vorbereitung für die Firebase-Implementierung

--- database/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def connect_db():
-    return mysql.connector.connect(
-        host="localhost", # host?
-        user="admin",
-        password="4Cgebaerden",
-        database="gebaerdentrainer"
-    )
