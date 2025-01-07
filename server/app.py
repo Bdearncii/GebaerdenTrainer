60b8f752c00fc1064549573aa341178f1bcf0a51
Mahdi Afzali <210099@studierende.htl-donaustadt.at>
2025-01-07 19:30:56 +0100

    Implementierung von Firebase, Imports angepasst und in der requirements.txt Datei hinzugefügt

--- server/app.py
+++ server/app.py
@@ -1,11 +1,16 @@
 import sys
 import os
+import firebase_admin
 #Working directory wird auf den Übergeordnetenordner von Server geändert.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from server.blueprints.usermanagement.routes import signup_bp, login_bp
-from database.models import add_user, get_user_by_username, add_progress, get_progress_by_user, add_video, get_video_by_description
+from firebase_admin import db, credentials
+
+cred = credentials.Certificate("credentials.json") # Credentials der Datenbank
+firebase_admin.initialize_app(cred, {"databaseURL": "https://gebaerdentrainer-default-rtdb.europe-west1.firebasedatabase.app"}) # Initializierung der Datenbank
+ref = db.reference('/')
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -16,48 +21,50 @@ app.register_blueprint(login_bp)
 
 @app.route('/add_user', methods=['POST'])
 def add_user_route():
-    data = request.get_json()
-    add_user(data['username'], data['email'], data['password'])
+    params = request.get_json()
+    user_ref = db.reference('users')
+    #user_count = user_ref.get() or 0  // Geben wir dem User eine ID? 
+    #user_count += 1
+    data={"username":params['user'],"email":params['email'],"password":params['password'],}
+    user_ref.push(data)
     return jsonify({"message": "User added successfully"}), 201 
 
 
-@app.route('/get_user/<username>', methods=['GET'])
-def get_user_route(username):
-    user = get_user_by_username(username)
-    if user:
-        return jsonify({"userid": user[0],
-                        "username": user[1],
-                        "email": user[2]
-        })
-    else:
-        return jsonify({"message": "User not found"}),404
 
 
-@app.route('/add_progress',methods=['POST'])
-def add_progress_route():
-    data = request.get_json()
-    add_progress(data['user_id'], data['sprache'], data['completion_date'])
-    return jsonify({"message": "Progress added successfully"}), 201
 
+# @app.route('/add_progress',methods=['POST'])
+# def add_progress_route():
+#     params = request.get_json()
+#     progress_ref = db.reference('progress') 
+#     add_progress(data['user_id'], data['sprache'], data['completion_date'])
+#     return jsonify({"message": "Progress added successfully"}), 201
 
-@app.route('/get_progress/<user_id>', methods=['GET'])
-def get_progress_route(user_id):
-    progress = get_progress_by_user(user_id)
-    return jsonify(progress)
+# Wie sollen wir Progress speichern? Intern beim User?
 
 
 
 @app.route('/add_video', methods=['POST'])
 def add_video_route():
-    data = request.get_json()
-    add_video(data['fach'], data['video_url'], data['video_description'])
+    params = request.get_json()
+    video_ref = db.reference('videos')
+    data={"Sprache": params['lang'], "URL": params['url'], "Description": params['description']}
+    video_ref.push(data)
     return jsonify({"message": "Video added successfully"}), 201
 
 
-@app.route('/add_video/<description>', methods=['POST'])
+@app.route('/get_video/<description>', methods=['GET'])
 def get_video_route(video_description):
-    video = get_video_by_description(video_description)
-    return jsonify(video)
+    params = request.get_json()
+    video_ref = db.reference('videos')
+    for video in video_ref: # durch die individuellen Videos werden durchgegangen
+        if video['description'] == params['video_description']:
+                return jsonify(video)
+
+    return jsonify({"message": "Video does not exist!"})
+            
+
+
 
 
 if __name__ == '__main__':
