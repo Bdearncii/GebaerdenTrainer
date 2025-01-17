fd0d48d08821634031b0daba5f0116f31cffeca6
Daniel Ferenci <210192@studierende.htl-donaustadt.at>
2025-01-17 16:04:55 +0100

    Kompatibilität mit vercel ermöglichen

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Basis-URL für die Bereitstellung auf Vercel
+  base: '/',
+
   plugins: [react()],
+
+  build: {
+    outDir: 'dist', // Der Output-Ordner für den Build
+  },
 })
