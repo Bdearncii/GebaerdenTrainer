f8af1debf9bba30cea5dfc9af95660f5e8a82b07
Daniel Ferenci <210192@studierende.htl-donaustadt.at>
2025-01-27 13:16:27 +0100

    Implementierung von einem hashalgorithmus zum sicheren speichern des Passworts

--- server/blueprints/usermanagement/routes.py
+++ server/blueprints/usermanagement/routes.py
@@ -1,10 +1,11 @@
-
 import firebase_admin
 from flask import Blueprint, jsonify, request
 from firebase_admin import db, credentials
+import bcrypt
 
-cred = credentials.Certificate("credentials.json") # Credentials der Datenbank
-firebase_admin.initialize_app(cred, {"databaseURL": "https://gebaerdentrainer-default-rtdb.europe-west1.firebasedatabase.app"}) # Initializierung der Datenbank
+cred = credentials.Certificate("credentials.json")  # Credentials der Datenbank
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://gebaerdentrainer-default-rtdb.europe-west1.firebasedatabase.app"})  # Initializierung der Datenbank
 ref = db.reference('/')
 
 signup_bp = Blueprint('signup', __name__)
@@ -12,21 +13,22 @@ login_bp = Blueprint('login', __name__)
 
 users = {}
 
+
 # Todo: Firebase Integration
 @signup_bp.route('/signup', methods=['POST'])
 def signup():
     # Empfange JSON-Daten vom Client
     data = request.get_json()
 
-    #Referenz auf die Datenbank und explizit auf die User-Tabelle
+    # Referenz auf die Datenbank und explizit auf die User-Tabelle
     user_ref = db.reference('users')
 
-
     # Benutzername und Passwort aus dem Request erhalten
-    email=data.get('email')
+    email = data.get('email')
     username = data.get('username')
     password = data.get('password')
 
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     if username in users:
         # Benutzername exestiert bereits
@@ -35,10 +37,11 @@ def signup():
     # Registrierung erfolgreich
     users[username] = password
 
-    #Hinzufügen des Users in die Datenbank
-    dbdata={"username":username,"password":password}
+    # Hinzufügen des Users in die Datenbank
+    dbdata = {"username": username, "password": hashed}
     user_ref.push(data)
     return jsonify({"message": "Successfull SignUp"}), 201
+
 
 # Todo: Firebase Integration
 @login_bp.route('/login', methods=['POST'])
@@ -46,9 +49,8 @@ def login():
     # Empfange JSON-Daten vom Client
     data = request.get_json()
 
-    #Referenz von Datenbank
+    # Referenz von Datenbank
     user_ref = db.reference('users')
-
 
     # Überprüfen, ob Benutzername und Passwort im Request enthalten sind
     username = data.get('username')
@@ -56,17 +58,15 @@ def login():
 
     user = user_ref.get(username)
 
-    #Direkter Vergleich in der Datenbank
-    if username == user['username'] and password == user['password']:
+    # Direkter Vergleich in der Datenbank
+    if username == user['username'] and bcrypt.checkpw(password, user['password']):
         return jsonify({"message": f"Welcome, {username}!"}), 202
     else:
-         return jsonify({"error": "Invalid username or password"}), 401
-
-
-    if username in users and users[username] == password:
-        # Erfolgreicher Login, sende JSON-Antwort mit Erfolg
-        return jsonify({"message": f"Welcome, {username}!"}), 202
-    else:
-        # Ungültige Anmeldedaten, sende Fehlerantwort
         return jsonify({"error": "Invalid username or password"}), 401
 
+    # if username in users and users[username] == password:
+    #     # Erfolgreicher Login, sende JSON-Antwort mit Erfolg
+    #     return jsonify({"message": f"Welcome, {username}!"}), 202
+    # else:
+    #     # Ungültige Anmeldedaten, sende Fehlerantwort
+    #     return jsonify({"error": "Invalid username or password"}), 401
